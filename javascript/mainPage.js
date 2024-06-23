c007c871682eb8bec106eeebbbe7f57a1f8831de
BABO00 <dntjd5901@naver.com>
2024-06-23 12:21:28 +0900

    0622

---
+++ javascript/mainPage.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  $(".slideContentWrapper").each(function () {
+    let $wrapper = $(this);
+    console.log($wrapper);
+    let $contentSlider = $wrapper.find(".contentBoxs");
+    let $leftButton = $wrapper.find("#leftButton");
+    let $rightButton = $wrapper.find("#rightButton");
+    let currentIndex = 0;
+    let slideWidth = $wrapper.width();
+
+    function slide() {
+      let translateValue = -slideWidth * currentIndex + "px";
+      $contentSlider.css("transform", `translateX(${translateValue})`);
+    }
+
+    $leftButton.on("click", function () {
+      if (currentIndex > 0) {
+        currentIndex--;
+        slide();
+      }
+    });
+
+    $rightButton.on("click", function () {
+      let maxIndex = Math.ceil($contentSlider[0].scrollWidth / slideWidth) - 1;
+      console.log(maxIndex);
+      if (currentIndex < maxIndex) {
+        currentIndex++;
+        slide();
+      }
+    });
+  });
+});
