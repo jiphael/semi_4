e79b784227ea0fe0d1559cb07c0ab3da43dc7901
BABO00 <dntjd5901@naver.com>
2024-06-24 17:52:45 +0900

    0624

--- javascript/mainPage.js
+++ javascript/mainPage.js
@@ -1,3 +1,4 @@
+// 슬라이드 바
 $(document).ready(function () {
   $(".slideContentWrapper").each(function () {
     let $wrapper = $(this);
@@ -20,7 +21,7 @@ $(document).ready(function () {
     });
 
     $rightButton.on("click", function () {
-      let maxIndex = Math.ceil($contentSlider[0].scrollWidth / slideWidth) - 2;
+      let maxIndex = Math.ceil($contentSlider[0].scrollWidth / slideWidth) - 1;
       if (currentIndex < maxIndex) {
         currentIndex++;
         slide();
@@ -28,3 +29,37 @@ $(document).ready(function () {
     });
   });
 });
+
+// 카테고리 바
+
+$("#showCategoryBar").on("click", function(){
+  console.log("ddd")
+  if ($(".categoryBar").css("display") == "none"){
+    $(".categoryBar").slideDown(200);
+  } else {
+    $(".categoryBar").slideUp(200);
+  }
+})
+
+// 무한 슬라이드
+$(function(){
+    var $slides = $(this).find('li');
+    const $contentSlider = $(".infinitySlider > ul");
+    const slideCount = $slides.length;
+    let currentIndex = 0;
+    const slideWidth = 1280;
+    
+    setInterval(showNextSlide, 2000);
+
+    function showNextSlide() {
+        var nextIndex = (currentIndex + 1) % slideCount;
+  
+        let translateValue = -slideWidth * currentIndex + "px";
+        $contentSlider.css("transform", `translateX(${translateValue})`);
+        currentIndex = nextIndex;
+      }
+  })
+  
+  
+
+
